ab129e6ba70e351a5f17b1a17f80ccbcbc16542e
Alejandro Valencia <alejandrov.net@gmail.com>
2024-04-06 15:55:36 -0500

    Se crea api en express para insertar las entidades usando express

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-const users = require('./users');
-const companies = require('./companies');
-const challenges = require('./challenges');
+const express = require('express');
+const seed = require('./init');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 
-const run = async () => {
-    // Se insertan los usuarios 
-    const listId = await users();
-    // if (listId.length === 0) {
-    //     console.log('############## Insert users #####################');
-    //     console.log('No se insertaron usuarios');
-    //     console.log('############## Insert users #####################');
-    //     return;
-    // }
-    // // Se insertan las companies
-    // await companies(listId);
+// Ruta para obtener todos los usuarios
+app.get('/api/gpt', async (req, res) => {
+  const data = await seed();
+  res.json({ msg: 'lista de users', data });
+});
 
-    // // Se insertan las challenges
-    // await challenges(listId);
-
-}
-
-run();
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado en el puerto ${PORT}`);
+});
--- src/util.js
+++ src/util.js
@@ -6,6 +6,7 @@ const openai = new OpenAI({
     apiKey: process.env.CHAT_GPT_API_KEY
 });
 
+// Se pide a chat gpt que cree los objetos de las entidades
 const generate = async (text) => {
     const prompt = text;
     const completion = await openai.completions.create({
@@ -18,32 +19,52 @@ const generate = async (text) => {
     return completion.choices[0].text.trim();
 };
 
-const execute = async (text) => {
-    const response = await generate(text);
-    console.log('**************** execute util *******************');
-    console.log(text);
-    console.log(response);
-    console.log('**************** execute util *******************');
-    const jresponse = JSON.parse(response);
-    return jresponse;
+// Se ejecuta la api de chat gpt y se retorna el json del objecto a insertar de la entidad
+const run = async (text) => {
+    try {
+        const response = await generate(text);
+        console.log('**************** execute util *******************');
+        console.log(text);
+        console.log(response);
+        console.log('**************** execute util *******************');
+        const jresponse = JSON.parse(response);
+        return jresponse;
+    } catch (e) {
+        console.log('.................. util 31 ...........');
+        console.log(e);
+        console.log('......................................');
+        execute(text);
+    }
 };
 
-const run = async (text) => {
-    // for (let i = 0; i < process.env.MAX_LOOP; i++) {
-    let result = await execute(text);
-    console.log(':::::::::::::::::::: run util ::::::::::::::');
-    console.log(text);
-    console.log(result);
-    console.log(':::::::::::::::::::: run util ::::::::::::::');
-    // }
-    return result;
-}
+// const run = async (text) => {
+//     let result = await execute(text);
+//     console.log(':::::::::::::::::::: run util ::::::::::::::');
+//     console.log(text);
+//     console.log(result);
+//     console.log(':::::::::::::::::::: run util ::::::::::::::');
+//     if (result !== undefined) {
+//         return result;
+//     }
+//     run(text);
+// }
 
+// Se obtiene la respuesta de chat gpt y se consume la api en laravel de las entidades
 const insertApi = async ({ text, urlApi }) => {
-    const datosChatGpt = await run(text);
-    return await Promise.all(datosChatGpt.map(data =>
-        axios.post(urlApi, data)
-    ));
+    try {
+        const datosChatGpt = await run(text);
+        if (datosChatGpt !== undefined) {
+            console.log(' ------------------------------- insertApi --------------------------');
+            console.log(datosChatGpt);
+            console.log(' ------------------------------- insertApi --------------------------');
+            return await Promise.all(datosChatGpt.map(data =>
+                axios.post(urlApi, data)
+            ));
+        }
+        insertApi({ text, urlApi });
+    } catch (e) {
+        insertApi({ text, urlApi });
+    }
 }
 
 module.exports = { insertApi, run };
